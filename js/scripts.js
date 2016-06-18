b385a10fabfb69dae9debad101ae837b93ed70b4
Oskar <oskarradonkimball@gmail.com>
2016-06-18 15:19:02 -0700

    js works

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,16 @@ $(function() {
   $(".popup").hide();
   $('a').click(function(e) {
     e.preventDefault();
-    $('.popup').show();
-  })
+    var classname = $(this).attr('class');
+    $('.popup.' + classname).show();
+  });
 
-  // // show div with class === link class
-  // $('a').click(function(e){
-  //   e.preventDefault();
-  // var classname = $(this).attr('class');
-  //   $('.popup.' + classname).css({ transition: '1s ease-in-out', opacity: 1 });
-  // });
-  //
-  // // hide div w class === x link class
-  // $('.x').click(function(e) {
-  //   e.preventDefault();
-  //   var classes = $(this).attr('class');
-  //   var classname = classes.split(' ')[1];
-  //   $('.popup.' + classname).css({ transition: '1s ease-in-out', opacity: 0 });
-  // });
+  // hide div w class === x link class
+  $('.x').click(function(e) {
+    e.preventDefault();
+    var classes = $(this).attr('class');
+    var classname = classes.split(' ')[1];
+    $('.popup.' + classname).hide();
+  });
 
 });

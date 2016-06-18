baf12ce84e30718d2fafaa65e6ecde776fb6e4bb
Oskar <oskarradonkimball@gmail.com>
2016-06-18 15:12:43 -0700

    js isn't working

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(function() {
+  $(".popup").hide();
+  // show div with class === link class
+  $('a').click(function(e){
+    e.preventDefault();
+  var classname = $(this).attr('class');
+    $('.popup.' + classname).css({ transition: '1s ease-in-out', opacity: 1 });
+  });
+
+  // hide div w class === x link class
+  $('.x').click(function(e) {
+    e.preventDefault();
+    var classes = $(this).attr('class');
+    var classname = classes.split(' ')[1];
+    $('.popup.' + classname).css({ transition: '1s ease-in-out', opacity: 0 });
+  });
+
+});

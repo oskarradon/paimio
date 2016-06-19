207562bea8052138fd2f735173577656ad770bb1
Oskar <oskarradonkimball@gmail.com>
2016-06-18 18:14:15 -0700

    Randomize positioning of popup

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,31 @@
-$(function() {
-  // $(".popup").hide();
-  // $('a').click(function(e) {
-  //   e.preventDefault();
-  //   var classname = $(this).attr('class');
-  //   $('.popup.' + classname).show();
-  // });
-  //
-  // // hide div w class === x link class
-  // $('.x').click(function(e) {
-  //   e.preventDefault();
-  //   var classes = $(this).attr('class');
-  //   var classname = classes.split(' ')[1];
-  //   $('.popup.' + classname).hide();
-  // });
-  $('.link1').dialog({
-    autoOpen: false
-  })
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
+$(function() {
+  var z = 10; // variable for z-index
+
+  $(".popup").hide();
   $('a').click(function(e) {
     e.preventDefault();
-    $('div.link1').dialog('open');
+
+    var windowHeight = $(window).height();
+    var windowWidth = $(window).width();
+    var classname = $(this).attr('class');
+
+    $('.popup.' + classname).show().css({
+      'z-index' : z,
+      'left' : randomNumber(1, (windowWidth - 300)) + 'px',
+      'top' : randomNumber(1, (windowHeight - 600)) + 'px'
+    });
+    z += 1;
   });
 
+  // hide div w class === x link class
+  $('.x').click(function(e) {
+    e.preventDefault();
+    var classes = $(this).attr('class');
+    var classname = classes.split(' ')[1];
+    $('.popup.' + classname).hide();
+  });
 });

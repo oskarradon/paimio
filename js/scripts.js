c3773e02061362c69d66c31dda9e87b65d8a2502
Oskar <oskarradonkimball@gmail.com>
2016-06-19 11:25:49 -0700

    shouts out to Taufik Nurrohman

--- js/scripts.js
+++ js/scripts.js
@@ -31,30 +31,59 @@ $(function() {
   });
 
   // make divs draggable
+  // code by Taufik Nurrohman
 
-  function endMove() {
-    $(this).removeClass('movable');
-}
-  function startMove() {
-    $('.movable').on('mousemove', function(event) {
-        var thisX = event.pageX - $(this).width() / 2,
-            thisY = event.pageY - $(this).height() / 2;
+  $.fn.drags = function(opt) {
 
-        $('.movable').offset({
-            left: thisX,
-            top: thisY
-        });
-    });
-  }
+          opt = $.extend({
+              handle: "",
+              cursor: "move",
+              draggableClass: "draggable",
+              activeHandleClass: "active-handle"
+          }, opt);
 
-  $(".popup").on('mousedown', function() {
-      $(this).addClass('movable');
-      startMove();
-  }).on('mouseup', function() {
-      $(this).removeClass('movable');
-      endMove();
-  });
+          var $selected = null;
+          var $elements = (opt.handle === "") ? this : this.find(opt.handle);
+
+          $elements.css('cursor', opt.cursor).on("mousedown", function(e) {
+              if(opt.handle === "") {
+                  $selected = $(this);
+                  $selected.addClass(opt.draggableClass);
+              } else {
+                  $selected = $(this).parent();
+                  $selected.addClass(opt.draggableClass).find(opt.handle).addClass(opt.activeHandleClass);
+              }
+              var drg_h = $selected.outerHeight(),
+                  drg_w = $selected.outerWidth(),
+                  pos_y = $selected.offset().top + drg_h - e.pageY,
+                  pos_x = $selected.offset().left + drg_w - e.pageX;
+              $(document).on("mousemove", function(e) {
+                  $selected.offset({
+                      top: e.pageY + pos_y - drg_h,
+                      left: e.pageX + pos_x - drg_w
+                  });
+              }).on("mouseup", function() {
+                  $(this).off("mousemove"); // Unbind events from document
+                  if ($selected !== null) {
+                      $selected.removeClass(opt.draggableClass);
+                      $selected = null;
+                  }
+              });
+              e.preventDefault(); // disable selection
+          }).on("mouseup", function() {
+              if(opt.handle === "") {
+                  $selected.removeClass(opt.draggableClass);
+              } else {
+                  $selected.removeClass(opt.draggableClass)
+                      .find(opt.handle).removeClass(opt.activeHandleClass);
+              }
+              $selected = null;
+          });
+
+          return this;
+
+      };
 
 
-
+      $('.popup').drags();
 });

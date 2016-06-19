054bbb0a89f8cc32118283388ada9951a65133aa
Oskar <oskarradonkimball@gmail.com>
2016-06-19 11:22:53 -0700

    Somewhat buggy way to drag divs around

--- js/scripts.js
+++ js/scripts.js
@@ -29,4 +29,32 @@ $(function() {
     var classname = classes.split(' ')[1];
     $('.popup.' + classname).hide();
   });
+
+  // make divs draggable
+
+  function endMove() {
+    $(this).removeClass('movable');
+}
+  function startMove() {
+    $('.movable').on('mousemove', function(event) {
+        var thisX = event.pageX - $(this).width() / 2,
+            thisY = event.pageY - $(this).height() / 2;
+
+        $('.movable').offset({
+            left: thisX,
+            top: thisY
+        });
+    });
+  }
+
+  $(".popup").on('mousedown', function() {
+      $(this).addClass('movable');
+      startMove();
+  }).on('mouseup', function() {
+      $(this).removeClass('movable');
+      endMove();
+  });
+
+
+
 });

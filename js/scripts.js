3b10db8b623e9e5add4172040a5676210daa59d4
Oskar <oskarradonkimball@gmail.com>
2016-06-21 13:16:30 -0700

    Add if else statement to handle external links

--- js/scripts.js
+++ js/scripts.js
@@ -5,24 +5,27 @@ function randomNumber(min, max) {
 $(function() {
   var z = 10; // variable for z-index
 
-  // $(".popup").hide();
   $('a').click(function(e) {
-    e.preventDefault();
-
     var windowHeight = $(window).height();
     var windowWidth = $(window).width();
     var classname = $(this).attr('class');
-
-    $('.popup.' + classname).show().css({
-      'opacity' : 1,
-      'z-index' : z,
-      'left' : randomNumber(1, (windowWidth - 300)) + 'px',
-      'top' : randomNumber(1, (windowHeight - 500)) + 'px'
-    });
-    z += 1;
+    if(classname === undefined) {
+      // do nothing lol
+      return
+    } else {
+      e.preventDefault();
+      $('.popup.' + classname).show().css({
+        'opacity' : 1,
+        'z-index' : z,
+        'left' : randomNumber(1, (windowWidth - 300)) + 'px',
+        'top' : randomNumber(1, (windowHeight - 500)) + 'px'
+      });
+      z += 1;
+    }
   });
 
   // hide div w class === x link class
+  
   $('.x').click(function(e) {
     e.preventDefault();
     var classes = $(this).attr('class');
